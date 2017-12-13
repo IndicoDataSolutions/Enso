7f46b2274cb8958441b1236e8fd7ac39d546a732
Maritza EBLING <maritza@mylestone.com>
2017-12-13 17:52:39 -0500

    Use numpy

--- enso/sample/__init__.py
+++ enso/sample/__init__.py
@@ -30,38 +30,20 @@ class Random(Sampler):
         return numpy.random.choice(self.train_indices, self.train_size, replace=False)
 
 
-class PointWithDistance(object):
-    def __init__(self, index, distance):
-        self.index = index
-        self.distance = distance
-
-
 class KCenter(Sampler):
     @property
     def points(self):
         return [numpy.array(point) for point in self.data]
 
     def sample(self):
-        logging.info("Sampling for KCenter")
         random_center = numpy.random.choice(self.train_indices)
         centers = [random_center]
         while len(centers) < self.train_size:
-            min_distances = []
-            for index, point in enumerate(self.points):
-                distances = []
-                if index in centers:
-                    continue
-                distances = []
-                for center in centers:
-                    point_with_distance = PointWithDistance(index, self.distances[index][center])
-                    distances.append(point_with_distance)
-                new_min_point = min(distances, key=lambda point_with_dist: point_with_dist.distance)
-                min_distances.append(new_min_point)
-            new_center_point = max(min_distances,  key=lambda point_with_dist: point_with_dist.distance)
-            centers.append(new_center_point.index)
-            logging.info("Left to get {} for train size of {}".format(str(self.train_size - len(centers)), str(self.train_size)))
+            mins = numpy.min(self.distances[centers], axis=0)
+            center = numpy.argmax(mins)
+            centers.append(center)
         return centers
-
+    
     @property
     def distances(self):
         if not hasattr(self, '_distances'):
