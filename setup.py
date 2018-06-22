dd76ab692a692296ceee824ded190ef77429f788
Madison May <madison@indico.io>
2018-06-22 16:51:24 -0400

    ADD: don't rely on requirements file

--- setup.py
+++ setup.py
@@ -2,11 +2,20 @@
 import os
 from setuptools import setup, find_packages
 
-REQUIREMENTS = open('requirements.txt').readlines()
+REQUIREMENTS = [
+    'pandas>=0.20.3',
+    'seaborn>=0.8.1',
+    'tqdm>=4.19.4',
+    'IndicoIo>=1.1.5',
+    'scikit-learn>=0.18.0',
+    'numpy>=1.13.1',
+    'click>=6.7',
+    'bs4>=0.0.1',
+]
 
 setup(
     name='enso',
-    version='0.1.4',
+    version='0.1.5',
     description='Testing feature-sets and learning algorithms for transfer learning.',
     author='indico',
     author_email='engineering@indico.io',
