aa33836c882f59bba8273dfce844929a17374d61
benleetownsend <benlt@hotmail.co.uk>
2018-07-20 18:31:16 +0100

    FIX: Trying to overcome issues with enso and indico api

--- enso/experiment/finetuning.py
+++ enso/experiment/finetuning.py
@@ -4,7 +4,7 @@ import json
 import pandas as pd
 
 from indicoio.custom import Collection
-from finetune import LanguageModelClassifier, LanguageModelSequence
+from finetune import Classifier, SequenceLabeler
 from finetune.utils import finetune_to_indico_sequence, indico_to_finetune_sequence
 
 from enso.experiment import ClassificationExperiment
@@ -21,7 +21,7 @@ class Finetune(ClassificationExperiment):
     def __init__(self, *args, **kwargs):
         """Initialize internal classifier."""
         super().__init__(*args, **kwargs)
-        self.model = LanguageModelClassifier(autosave_path=os.path.join(RESULTS_DIRECTORY, '.autosave'))
+        self.model = Classifier(autosave_path=os.path.join(RESULTS_DIRECTORY, '.autosave'))
 
     def fit(self, X, y):
         """
@@ -39,35 +39,37 @@ class Finetune(ClassificationExperiment):
 class FinetuneSequenceLabel(ClassificationExperiment):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.model = LanguageModelSequence(autosave_path=os.path.join(RESULTS_DIRECTORY, '.autosave'))
+        self.model = SequenceLabeler(autosave_path=os.path.join(RESULTS_DIRECTORY, '.autosave'))
 
     def fit(self, X, y):
-        X = list(zip(X, [json.loads(y_) for y_ in y]))
-        data = indico_to_finetune_sequence(X, "<PAD>")
-        self.model.fit(data)
+        self.model.fit(X, y)
 
     def predict(self, X, **kwargs):
         predictions = self.model.predict(list(zip(*[X])))
-        return [json.dumps(targ[1]) for targ in finetune_to_indico_sequence(predictions, "<PAD>")]
+        return [json.dumps(targ) for targ in predictions]
 
 
 class IndicoSequenceLabel(ClassificationExperiment):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.model = Collection("Enso-Sequence-Labeling")
+        self.model = None
+
+    def fit(self, X, y):
+        self.model = Collection("Enso-Sequence-Labeling-{}".format(str(hash(str(X)+str(y)))))
         try:
             self.model.clear()
         except:
             pass
-
-    def fit(self, X, y):
         X = list(zip(X, [json.loads(y_) for y_ in y]))
         for x in X:
             self.model.add_data([x])
-            break
         self.model.train()
         self.model.wait()
 
     def predict(self, X, **kwargs):
         predictions = self.model.predict(X)
         return [json.dumps(targ) for targ in predictions]
+
+    def __del__(self):
+        self.model.clear()
+
