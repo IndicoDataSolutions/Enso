578bca5bd02fe9f82f7b17d7c66866f862191ee5
lily <lilyhzhang1029@gmail.com>
2020-04-13 11:37:50 -0400

    ADD: config for sidekick

--- enso/config.py
+++ enso/config.py
@@ -56,7 +56,8 @@ DATA = {
     # "Classify/SST-binary"
     # Seqence
     # 'SequenceLabeling/Reuters-128',
-    'SequenceLabeling/table_synth',
+    'SequenceLabeling/bonds',
+    # 'SequenceLabeling/table_synth',
     # 'SequenceLabeling/bonds_new',
     # 'SequenceLabeling/tables',
     # 'SequenceLabeling/typed_cols',
@@ -98,6 +99,7 @@ FEATURIZERS = {
 EXPERIMENTS = {
     # "FinetuneSequenceLabel",
     "RoBERTaSeqLab",
+    "SidekickSeqLab",
     # "IndicoSequenceLabel"
     # "LRBaselineNonRationalized",
     # "DistReweightedGloveClassifierCV",
@@ -190,39 +192,39 @@ indicoio.config.api_key = ""
 # EXPERIMENT_PARAMS = {}
 
 # For testing
-EXPERIMENT_PARAMS = {
-    'All': {"lr_warmup": [0.1, 0.2]}
-}
-
 # EXPERIMENT_PARAMS = {
-    # 'All': {
-    #     "lr_warmup": [0.1, 0.2],
-    #     "lr": [1e-5, 1e-4],
-    #     "batch_size": [8, 16],
-    #     "n_epochs": [16, 32],
-    # },
-#     'RoBERTaSeqLab': {
-#         'base_model_path': [
-#             "roberta-model-sm-v2.jl",
-#             # "filtered_mlm_baseline.jl",
-#             # "filtered_mlm_baseline_2nd_5.jl",
-#             "filtered_mlm_baseline_3rd_5.jl"
-#         ]
-#     },
-#     'LambertSeqLab': {
-#         'base_model_path': [
-#             # "filtered_lambert_mlm.jl",
-#             # "filtered_lambert_mlm_2nd_5.jl",
-#             "filtered_lambert_mlm_3rd_5.jl",
-#             # "filtered_lambert_mlm_pos_removal.jl"
-#         ]
-#     },
-#     'SidekickSeqLab': {
-#         'base_model_path': [
-#             # "filtered_sidekick_mlm.jl",
-#             # "filtered_sidekick_mlm_2nd_5.jl",
-#             "filtered_sidekick_mlm_3rd_5.jl",
-#             # "sidekick_mlm_pos_removal.jl"
-#         ]
-#     }
+#     'All': {"lr_warmup": [0.1, 0.2]}
 # }
+
+EXPERIMENT_PARAMS = {
+    'All': {
+        "lr_warmup": [0.1, 0.2],
+        "lr": [1e-5, 1e-4],
+        "batch_size": [8, 16],
+        "n_epochs": [16, 32],
+    },
+    'RoBERTaSeqLab': {
+        'base_model_path': [
+            "roberta-model-sm-v2.jl",
+            # "filtered_mlm_baseline.jl",
+            # "filtered_mlm_baseline_2nd_5.jl",
+            "filtered_mlm_baseline_3rd_5.jl"
+        ]
+    },
+    'LambertSeqLab': {
+        'base_model_path': [
+            # "filtered_lambert_mlm.jl",
+            # "filtered_lambert_mlm_2nd_5.jl",
+            "filtered_lambert_mlm_3rd_5.jl",
+            # "filtered_lambert_mlm_pos_removal.jl"
+        ]
+    },
+    'SidekickSeqLab': {
+        'base_model_path': [
+            # "filtered_sidekick_mlm.jl",
+            # "filtered_sidekick_mlm_2nd_5.jl",
+            "filtered_sidekick_mlm_3rd_5.jl",
+            # "sidekick_mlm_pos_removal.jl"
+        ]
+    }
+}
