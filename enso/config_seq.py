54ad78116f7fa5ccc10f7ce884830560a180cc07
lily <lilyhzhang1029@gmail.com>
2020-04-20 15:25:20 -0400

    ADD: seq and doc rep configs

--- enso/config_seq.py
+++ enso/config_seq.py
@@ -14,7 +14,7 @@ RESULTS_DIRECTORY = "Results"
 FEATURES_DIRECTORY = "Features"
 
 # Directory for storing experiment results
-EXPERIMENT_NAME = "DocRep"
+EXPERIMENT_NAME = "Exp"
 
 # Name of the csv used to store results
 RESULTS_CSV_NAME = "Results.csv"
@@ -56,6 +56,11 @@ DATA = {
     # "Classify/SST-binary"
     # Seqence
     'SequenceLabeling/Reuters-128',
+    # 'SequenceLabeling/bonds',
+    # 'SequenceLabeling/table_synth',
+    # 'SequenceLabeling/bonds_new',
+    # 'SequenceLabeling/tables',
+    # 'SequenceLabeling/typed_cols',
     # 'SequenceLabeling/brown_all',
     # 'SequenceLabeling/brown_nouns',
     # 'SequenceLabeling/brown_verbs',
@@ -76,6 +81,7 @@ DATA = {
 # Featurizers to activate
 FEATURIZERS = {
     "PlainTextFeaturizer",
+    # "TextContextFeaturizer",
     # "IndicoStandard",
     # "SpacyGloveFeaturizer",
     # "IndicoFastText",
@@ -93,6 +99,7 @@ FEATURIZERS = {
 EXPERIMENTS = {
     "FinetuneSequenceLabel",
     # "RoBERTaSeqLab",
+    # "SidekickSeqLab",
     # "IndicoSequenceLabel"
     # "LRBaselineNonRationalized",
     # "DistReweightedGloveClassifierCV",
@@ -100,21 +107,21 @@ EXPERIMENTS = {
     # 'DistReweightedGloveClassifierCV'
     # "FinetuneSeqBaselineRationalized",
     # "FinetuneClfBaselineNonRationalized",
-#    "LogisticRegressionCV",
-#    "KNNCV",
-#    "TfidfKNN",
-#    "TfidfLogisticRegression",
-#    "KCenters",
-#    "TfidfKCenters"
+    # "LogisticRegressionCV",
+    # "KNNCV",
+    # "TfidfKNN",
+    # "TfidfLogisticRegression",
+    # "KCenters",
+    # "TfidfKCenters"
     # "SupportVectorMachineCV",
 }
 
 # Metrics to compute
 METRICS = {
-#    "Accuracy",
+    # "Accuracy",
     # "AccuracyRationalized",
     # "MacroRocAucRationalized",
-#    "MacroRocAuc",
+    # "MacroRocAuc",
     "MacroCharF1",
     "MacroCharRecall",
     "MacroCharPrecision"
@@ -125,8 +132,8 @@ TEST_SETUP = {
     "train_sizes": [20, 40, 60, 80, 100, 150, 200, 300, 400, 500],
     "n_splits": 5,
     # "samplers": ['RandomRationalized'],
-#    "samplers": ["ImbalanceSampler"],
-    "samplers": ["RandomSequence"],
+    # "samplers": ["ImbalanceSampler"],
+    "samplers": ['RandomSequence'],
     "sampling_size": 0.2,
     "resamplers": ['NoResampler']
 #    "resamplers": ["RandomOverSampler"],
@@ -144,7 +151,8 @@ VISUALIZATION_OPTIONS = {
         "y_tile": "Dataset",
         "x_axis": "TrainSize",
         "y_axis": "Result",
-        "lines": ["Experiment", "Featurizer", "Sampler", "Resampler"],
+        "lines": ["Experiment", "Featurizer", "Sampler", "Resampler",
+                  "lr", "lr_warmup", "batch_size", "n_epochs", "base_model_path"],
         "category": "merge",
         "cv": "mean",
         "filename": "TestResult",
@@ -153,7 +161,7 @@ VISUALIZATION_OPTIONS = {
 
 MODE = ModeKeys.SEQUENCE
 
-N_GPUS = 0
+N_GPUS = 1
 N_CORES = 1  # multiprocessing.cpu_count()
 
 FIX_REQUIREMENTS = True
@@ -163,35 +171,5 @@ CORRUPTION_FRAC = 0.4
 
 indicoio.config.api_key = ""
 
-EXPERIMENT_PARAMS = {
-    'All': {
-        "lr_warmup": [0.1, 0.2],
-        "lr": [1e-5, 1e-4],
-        "batch_size": [8, 16],
-        "n_epochs": [16, 32],
-    },
-    'RoBERTaSeqLab': {
-        'base_model_path': [
-            "roberta-model-sm-v2.jl",
-            "mlm_baseline.jl",
-            "mlm_baseline_2nd_5.jl",
-            "mlm_baseline_3rd_5.jl"
-        ]
-    },
-    'LambertSeqLab': {
-        'base_model_path': [
-            "lambert_mlm.jl",
-            "lambert_mlm_2nd_5.jl",
-            "lambert_mlm_3rd_5.jl",
-            "lambert_mlm_pos_removal.jl"
-        ]
-    },
-    'SidekickSeqLab': {
-        'base_model_path': [
-            "sidekick_mlm.jl",
-            "sidekick_mlm_2nd_5.jl",
-            "sidekick_mlm_3rd_5.jl",
-            "sidekick_mlm_pos_removal.jl"
-        ]
-    }
-}
+# If we have no experiment hyperparameters we hope to modify:
+EXPERIMENT_PARAMS = {}
